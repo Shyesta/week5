bef66a2a3d585d43b8a2dc2a93786a75fcf42398
Sage Bain <bain.sage@student.greenriver.edu>
2024-02-01 13:48:17 -0500

    Added fat-free variables for testing

--- index.php
+++ index.php
@@ -9,10 +9,19 @@ require_once('vendor/autoload.php');
 
 $f3 = Base::instance();
 
-$f3->route('GET /', function () {
+$f3->route('GET /', function ($f3) {
+
+    //add key-value pair to the F3 hive
+    $f3->set("username", "sbain");
+    $f3->set("password", sha1("Password01"));
+    $f3->set("title", "Working with Templates");
+    $f3->set("temp", 67);
+    $f3->set("color", "purple");
+    $f3->set("radius", 10);
+    $f3->set("fruits", array("apple", "orange", "banana"));
     //Display a view page
     $view = new Template();
-    echo $view->render('views/home.html');
+    echo $view->render('views/info.html');
 });
 
 $f3->run();
