edcd35eca5d4681f761a1b4276d766feabe137e7
Sage Bain <bain.sage@student.greenriver.edu>
2024-02-01 12:13:58 -0500

    Added basic viewpage and config files

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+
+
+// Turn on error reporting
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
+require_once('vendor/autoload.php');
+
+$f3 = Base::instance();
+
+$f3->route('GET /', function () {
+    //Display a view page
+    $view = new Template();
+    echo $view->render('views/home.html');
+});
+
+$f3->run();
